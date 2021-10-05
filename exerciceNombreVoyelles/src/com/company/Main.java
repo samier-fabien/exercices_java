ba6363d2e0d95976d5c804ffd0bdfbea9499ea7f
Samier fabien <samierfabien@gmail.com>
2021-10-05 16:12:05 +0200

    ajout exercice voyelles

--- exerciceNombreVoyelles/src/com/company/Main.java
+++ exerciceNombreVoyelles/src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String mot = new String();
+        int nombreDeVoyelles = 0;
+        char caractere;
+        char[] tableauVoyelles = {'a', 'e', 'i', 'o', 'u', 'y'};
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bienvenue patati patata (v1.0 05/10/21)");
+
+        System.out.println("Veuillez saisir une mot :");
+        mot = scanner.next();
+
+        for (int i = 0; i < mot.length(); i++) {
+            caractere = mot.charAt(i);
+            for (int j = 0; j < tableauVoyelles.length; j++) {
+                if (caractere == tableauVoyelles[j]) {
+                    nombreDeVoyelles++;
+                }
+            }
+        }
+
+        System.out.println("Nombre de voyelle : "+nombreDeVoyelles);
     }
 }
