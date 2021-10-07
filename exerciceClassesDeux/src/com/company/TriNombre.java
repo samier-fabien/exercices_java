9fb45b897db45f1d5a077f33cf28f5188cec970f
Samier fabien <samierfabien@gmail.com>
2021-10-07 09:45:49 +0200

    Ajout exercices classes de base

--- exerciceClassesDeux/src/com/company/TriNombre.java
+++ exerciceClassesDeux/src/com/company/TriNombre.java
@@ -6,19 +6,49 @@ public class TriNombre {
     private int[] tableau;
     private int taille;
 
-    public TriNombre() {}
+    public TriNombre() {
+        this.saisie();
+        this.tri(tableau);
+        this.affiche();
+    }
 
     public void saisie() {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Veuillez donner une taille pour le tableau :");
+        taille = scanner.nextInt();
+
+        tableau = new int[this.getTaille()];
+
+        for (int i = 0; i < tableau.length; i++) {
+            System.out.println("Veuillez saisir le nombre "+(i+1));
+            tableau[i] = scanner.nextInt();
+        }
     }
 
-    public void tri() {
+    public void tri(int[] tableau) {
+        int echange;
+        boolean changement = true;
 
+        while (changement) {
+            changement = false;
+            for (int i = 0; i < tableau.length; i++) {
+                if (i < (tableau.length-1)) {
+                    if (tableau[i] > tableau[(i+1)]) {
+                        echange = tableau[i];
+                        tableau[i] = tableau[(i+1)];
+                        tableau[i+1] = echange;
+                        changement = true;
+                    }
+                }
+            }
+        }
     }
 
     public void affiche() {
-
+        for (int chiffre : tableau) {
+            System.out.println(chiffre);
+        }
     }
 
     public int[] getTableau() {
